6d9110864360323d47020cad8506f0b0651afd6e
FayazAliAtan <152722678+FayazAliAtan@users.noreply.github.com>
2025-01-29 23:21:24 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php 
+            $servername = "localhost"; // Replace with your server name or IP
+            $username = "root";        // Replace with your database username
+            $password = "";            // Replace with your database password
+            $database = "DocSphere"; // Replace with your database name
+            
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $database);
+    
+    
+    ?>
+    
